083044214559de60cc20892a96bb807725d92ee1
kamusone <benayad.kame@gmail.com>
2019-03-15 11:54:25 +0100

    add commentaire & push data_merged_per_job

--- merged_all_jobs.py
+++ merged_all_jobs.py
@@ -10,6 +10,7 @@ import nltk
 from nltk.corpus import stopwords
 import string
 
+#import & rename columns
 df_analyst = pd.read_csv('merged_data_analyst.csv')
 df_analyst.columns=["Titre","Nom","Adresse","Salaire","Contrat","Descpost","Date","Metier"]
 df_bi = pd.read_csv('merged_BI.csv')
@@ -22,13 +23,14 @@ df_developeur.columns=["Titre","Nom","Adresse","Salaire","Contrat","Descpost","D
 
 
 #merged all csv
-
 df_all = pd.concat([df_analyst,df_bi,df_scientist,df_developeur])
 
+#create csv
 df_all.to_csv("merged_4jobs.csv", encoding='utf-8', index=False)
 
 
 ## create a sub-df consisting only of jobs with annual salaries
-df_annual_salary=df_all[df_all.Salaire.notnull()&df_all.Salaire.str.contains('par an')]
-df_mensuel_salary=df_all[df_all.Salaire.notnull()&df_all.Salaire.str.contains('par mois')]
+df_annual_salary=df_all[df_all.Salaire.notnull()&df_all.Salaire.str.contains('par an')] # 1582 annual_salary
+
+df_mensuel_salary=df_all[df_all.Salaire.notnull()&df_all.Salaire.str.contains('par mois')]#203 mensuel_salary
 df_all.Salaire = df_all.Salaire.astype(str)
